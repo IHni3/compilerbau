0bec2736543b188fa3f5c2174c0e84168866336e
Tim Hönnige <timhoennige@gmx.de>
2021-02-04 19:03:29 +0100

    parse tree added

--- src/test/java/VisitorNFLTest.java
+++ src/test/java/VisitorNFLTest.java
@@ -4,6 +4,19 @@ import org.junit.jupiter.api.TestFactory;
 
 import java.util.stream.Stream;
 
+/*
+                °
+               / \
+              /   #
+             |
+            / \
+           °   \
+          / \   *
+         +   ?  |
+         |   |  C
+         A   B
+ */
+
 public class VisitorNFLTest {
 
     @TestFactory
